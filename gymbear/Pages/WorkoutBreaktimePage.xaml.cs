0244fb73e0d01108ee55a0bc825f3bfed1fee047
Valicsek David <entriek@gmail.com>
2018-09-09 14:36:34 +0200

    Modify the WorkoutBreakingTimePage

--- gymbear/Pages/WorkoutBreaktimePage.xaml.cs
+++ gymbear/Pages/WorkoutBreaktimePage.xaml.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using gymbear.ViewModels;
 using Xamarin.Forms;
 
 namespace gymbear.Pages
 {
     public partial class WorkoutBreaktimePage : ContentPage
     {
+        public WorkoutOnGameViewModel VM;
+
         public WorkoutBreaktimePage()
         {
             InitializeComponent();
+            this.VM = new WorkoutOnGameViewModel();
+            this.BindingContext = this.VM;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            SetupTheTimer();
+        }
+
+        void SetupTheTimer()
+        {
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                if (this.VM.BreakTimeLeft == 0) return false;
+                this.VM.BreakTimeLeft--;
+                return true;
+            });
+        }
+
+        void OnSkipButtonClicked(object sender, System.EventArgs e)
+        {
+            Navigation.PopModalAsync();
         }
     }
 }
