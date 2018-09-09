351b0ab218de16fcc8e7aa701bd1f7b8c9baa49b
Valicsek David <entriek@gmail.com>
2018-09-09 14:36:14 +0200

    Modify the WorkoutOnGamePage

--- gymbear/Pages/WorkoutOnGamePage.xaml.cs
+++ gymbear/Pages/WorkoutOnGamePage.xaml.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using gymbear.ViewModels;
 using Xamarin.Forms;
 
 namespace gymbear.Pages
 {
     public partial class WorkoutOnGamePage : ContentPage
     {
+        public WorkoutOnGameViewModel VM;
+
         public WorkoutOnGamePage()
         {
             InitializeComponent();
+            this.VM = new WorkoutOnGameViewModel();
+            this.BindingContext = this.VM;
+            SetupViews();
+        }
+
+        void SetupViews()
+        {
+            NavigationPage.SetHasBackButton(this, false);
+            this.Title = "Workout";
+        }
+
+        void OnNextExerciseButtonClicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushModalAsync(new WorkoutBreaktimePage());
+        }
+
+        void OnCancelButtonClicked(object sender, System.EventArgs e)
+        {
+            // Remove current Page.
+            Navigation.PopAsync();
         }
     }
 }
