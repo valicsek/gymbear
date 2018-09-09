e75260a2eaaae832ed059d7479c4534ff0999052
Valicsek David <entriek@gmail.com>
2018-09-09 14:36:22 +0200

    Modify the WorkoutPage

--- gymbear/Pages/WorkoutPage.xaml.cs
+++ gymbear/Pages/WorkoutPage.xaml.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using gymbear.ViewModel;
 using Xamarin.Forms;
 
 namespace gymbear.Pages
 {
     public partial class WorkoutPage : ContentPage
     {
+        public WorkoutModelView VM { get; set; }
+
         public WorkoutPage()
         {
             InitializeComponent();
+            this.VM = new WorkoutModelView();
+            this.BindingContext = this.VM;
+        }
+
+        void OnStartWorkoutButtonClicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushModalAsync(new WorkoutOnGamePage());
         }
     }
 }
