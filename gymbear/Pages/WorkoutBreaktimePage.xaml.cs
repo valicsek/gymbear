a3543ba4b84bf9b219c7f767e28122a35cd7e845
Valicsek David <entriek@gmail.com>
2018-09-10 10:57:02 +0200

    Add alert sound for iOS when the timer stop

--- gymbear/Pages/WorkoutBreaktimePage.xaml.cs
+++ gymbear/Pages/WorkoutBreaktimePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using AudioToolbox;
 using gymbear.ViewModels;
 using Xamarin.Forms;
 
@@ -8,12 +9,17 @@ namespace gymbear.Pages
     public partial class WorkoutBreaktimePage : ContentPage
     {
         public WorkoutOnGameViewModel VM;
+        private SystemSound systemSound;
 
         public WorkoutBreaktimePage()
         {
             InitializeComponent();
             this.VM = new WorkoutOnGameViewModel();
             this.BindingContext = this.VM;
+
+            // You can find more sound here:
+            // https://forums.xamarin.com/discussion/35751/how-the-hell-i-can-play-a-simple-ios-system-sound
+            this.systemSound = new SystemSound(1005);
         }
 
         protected override void OnAppearing()
@@ -28,12 +34,26 @@ namespace gymbear.Pages
             {
                 if (this.VM.BreakTimeLeft == 0)
                 {
+                    NotifyUserToStopRestWithSoundEffect();
                     Navigation.PopModalAsync();
                     return false;
                 }
                 this.VM.BreakTimeLeft--;
                 return true;
             });
+        }
+
+        //
+        // This function called when the timer has been finished
+        // It basically notifies the user.
+        // 
+        void NotifyUserToStopRestWithSoundEffect()
+        {
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                this.systemSound.PlayAlertSound();
+            }
+            // TODO: Add Android Alert sound as well.
         }
 
         void OnSkipButtonClicked(object sender, System.EventArgs e)
